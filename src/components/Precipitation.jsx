c567c3a7b9016a798efbdbd6fab690805648d18b
karnan7 <adithkayalad@gmail.com>
2023-07-01 23:33:22 +0530

    precipitation customize

--- src/components/Precipitation.jsx
+++ src/components/Precipitation.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Bar } from 'react-chartjs-2'
 
 const Precipitation = () => {
+
+    const data = {
+        labels: [],
+        datasets:  {
+            labels: [],
+            data: [],
+        }
+    }
+
+    const options = {
+
+    }
   return (
-    <div>Precipitation</div>
+    <div>
+        <Bar
+        data={data}
+        options={options}
+        />
+    </div>
   )
 }
 
